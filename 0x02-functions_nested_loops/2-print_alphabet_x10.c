5c375433f85af390e716d10867e1a095691580b8
Iwaami <bunminiwaruth@gmail>
2022-09-13 15:37:33 -0700

    Slower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Make alphabet x10 times
- * 
+ * print_alphabet_x10 - entry point
+ *
+ * Description: Prints the alphabet with _putchar ten times
+ *
  * Return: void
  */
+
+
 void print_alphabet_x10(void)
 {
 	char c;
-	int i = 0
+	int i;
 
-	while (i <= 9)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
-								
 		_putchar('\n');
-								
-		i++;
 	}
 }
